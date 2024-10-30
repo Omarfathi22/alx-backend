2fa9e2d843f6dab1833f7e0d96cded08d5e526b6
Omarfathi22 <ommarf525@gmail.com>
2024-10-30 20:09:24 +0300

    fix-0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python3
-"""Task 0: Basic dictionary
-This script defines a simple caching system
-using a basic dictionary.
-"""
+
+'''Task 0: Basic dictionary
+'''
+
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """A class `BasicCache` that inherits from `BaseCaching`.
-    This class implements a basic caching system where
-    items can be stored
-    and retrieved using keys. It uses a dictionary
-    to store the cached data.
-    """
+    '''A class `BasicCache` that inherits from `BaseCaching`
+       and is a caching system
+    '''
 
     def put(self, key, item):
-        """Assigns the value `item` to the dictionary
-        `self.cache_data` using the provided `key`.
-
-        Parameters:
-        key (str): The key under which the item will be stored.
-        item (Any): The value to be stored in the cache.
-        """
-        
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Retrieves the value associated with the provided
-        `key` from `self.cache_data`.
-        """
+        '''return the value in `self.cache_data` linked to `key`
+        '''
+
         return self.cache_data.get(key, None)
     
