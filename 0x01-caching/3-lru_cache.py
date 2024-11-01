db3a472197d97f94037853c6b919d05acbaf4a2a
Omarfathi22 <ommarf525@gmail.com>
2024-11-01 19:19:28 +0200

    fix -3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,59 +1,47 @@
 #!/usr/bin/env python3
-'''Task 3: LRU Caching
-'''
-
-from collections import OrderedDict
+""" BaseCaching module
+"""
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    '''A class `LRUCache` that inherits from `BaseCaching`.
-    This class implements a caching system that uses a 
-    Least Recently Used (LRU) eviction policy.
-    '''
-
+    """
+    LRUCache defines a Least Recently Used (LRU) caching system.
+    This cache evicts the least recently used item when the cache exceeds
+    its maximum size.
+    """
     def __init__(self):
-        '''Initializes the LRUCache instance and sets up
-        the cache data structure as an ordered dictionary.
-        '''
+        """
+        Initialize the LRUCache instance.
+        Calls the parent class's initializer and sets up a list to track
+        the usage order of keys.
+        """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.usage = []
 
     def put(self, key, item):
-        '''Adds an item to the cache under the specified key.
-        
-        If the cache exceeds the maximum allowed items,
-        the least recently used item is discarded.
-        
-        Parameters:
-        key (str): The key under which the item will be stored.
-        item (Any): The item to be stored in the cache.
-        '''
+        """
+        Cache a key-value pair
+        """
         if key is None or item is None:
-            return
-        
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(last=False)
-                print("DISCARD:", lru_key)
-        
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=False)
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.usage[0]))
+                del self.cache_data[self.usage[0]]
+                del self.usage[0]
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        '''Retrieves an item from the cache by its key.
-        
-        If the key exists, it marks the item as recently used.
-        
-        Parameters:
-        key (str): The key of the item to retrieve.
-        
-        Returns:
-        The value associated with the key, or None if the key does not exist.
-        '''
-        if key is not None and key in self.cache_data:
-
-            self.cache_data.move_to_end(key, last=False)
-        
-        return self.cache_data.get(key, None)
-    
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data.keys():
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            return self.cache_data[key]
+        return None
